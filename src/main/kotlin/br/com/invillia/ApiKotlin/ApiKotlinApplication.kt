c451b8513a744f4f4487d7c35b3a6a19503ded3e
AlineRibeiroBraga <aline_ribeirobraga@hotmail.com>
2020-06-08 14:49:19 -0300

    Primeiro push

---
+++ src/main/kotlin/br/com/invillia/ApiKotlin/ApiKotlinApplication.kt
@@ -0,0 +1,11 @@
+package br.com.invillia.ApiKotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiKotlinApplication>(*args)
+}
